de612ce57af1ab6938c157ec6e57bfa1b0abd278
krishnateja yarlagadda <kyarlagadda37@gmail.com>
2025-05-07 11:22:42 -0400

    Added CSV file to the manager and loaded with required file path

--- ExpenseManager.java
+++ ExpenseManager.java
@@ -14,4 +14,22 @@ public class ExpenseManager {
         return expenses;
     }
 
- 
+    public void saveToCSV(String filePath) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Expense expense : expenses) {
+                writer.write(expense.toCSV());
+                writer.newLine();
+            }
+        }
+    }
+
+    public void loadFromCSV(String filePath) throws IOException {
+        expenses.clear();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                expenses.add(Expense.fromCSV(line));
+            }
+        }
+    }
+}
